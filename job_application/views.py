5114fea6593ed467e45e02685be891862afed595
Piyush Jain <piyushjain100201@gmail.com>
2025-02-15 00:38:24 +0530

    add admin interface and navbar

--- job_application/views.py
+++ job_application/views.py
@@ -26,3 +26,10 @@ def index(request):
 
     return render(request,"index.html")
 
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
