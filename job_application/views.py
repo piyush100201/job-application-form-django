9fa780e3781a094d4d782101b15188117163b4dd
Piyush Jain <piyushjain100201@gmail.com>
2025-02-14 23:52:39 +0530

    store data in database and send confirmation email

--- job_application/views.py
+++ job_application/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from pyexpat.errors import messages
+
+from .forms import ApplicationForm
+from .models import Form
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        date = request.POST.get('available_date')
+        occupation = request.POST.get('occupation')
+
+
+        Form.objects.create(first_name = first_name, last_name = last_name,
+                            email = email, date = date, occupation = occupation)
+
+        message_body = f"A new job application was submitted. Thank You, {first_name}."
+        email_message = EmailMessage("Form Submission Confirmation", message_body,to=[email])
+        email_message.send()
+
     return render(request,"index.html")
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from django.conf.global_settings import EMAIL_USE_TLS, EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'piyushjain100201@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv("PASSWORD")
+
+
